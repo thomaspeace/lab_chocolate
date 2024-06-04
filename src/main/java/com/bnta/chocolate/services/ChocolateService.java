0630fb70e1ce46d552cc8fddd193625d231b4ffe
Sunny Mudhar <sunielmudhar@gmail.com>
2024-06-04 15:46:06 +0100

    Created services for Chocolate and Estate

---
+++ src/main/java/com/bnta/chocolate/services/ChocolateService.java
@@ -0,0 +1,22 @@
+package com.bnta.chocolate.services;
+
+import com.bnta.chocolate.models.Chocolate;
+import com.bnta.chocolate.models.Estate;
+import com.bnta.chocolate.repositories.ChocolateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ChocolateService {
+
+    @Autowired
+    ChocolateRepository chocolateRepository;
+
+    public List<Chocolate> getAllChocolates() {
+        return chocolateRepository.findAll();
+    }
+
+}
---
+++ src/main/java/com/bnta/chocolate/services/EstateService.java
@@ -0,0 +1,27 @@
+package com.bnta.chocolate.services;
+
+import com.bnta.chocolate.models.Estate;
+import com.bnta.chocolate.repositories.EstateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EstateService {
+
+    @Autowired
+    EstateRepository estateRepository;
+
+    public List<Estate> getAllEstates() {
+        return estateRepository.findAll();
+    }
+
+    // EXT - Get by ID
+    public Optional<Estate> getEstateById(Long id) {
+        return estateRepository.findById(id);
+    }
+
+    // EXT - Save
+}
